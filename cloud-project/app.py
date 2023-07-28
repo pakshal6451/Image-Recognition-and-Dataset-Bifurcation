e36aa2936cdb1231ed908f982ec1611339d71621
Pakshal Shah <49347161+pakshal6451@users.noreply.github.com>
2023-07-28 13:08:10 -0300

    Update app.py

--- cloud-project/app.py
+++ cloud-project/app.py
@@ -10,7 +10,7 @@ S3_ACCESS_KEY = 'ASIATCG46ZXROB6UUGNI'
 S3_SECRET_KEY = 't4o0tksHpyozGa6DsnfS3t6yNdOFq8ihgcO2/pBJ'
 S3_SESSION_TOKEN = 'FwoGZXIvYXdzEPD//////////wEaDKgZCTUzA937QNllKSLAATqCfoTl2AZSzXuq1aTLkDDQy8sR1uxtWWoKxVJ6+vOyA5sDIZVz8WZL2zVacaHbKcx6j0ND7uaTJdFNWVe8SAZXou+kpqk4VmS6mvb9aHxduThLkbGECeloDps1wcN6G/WTahOgsgjvVg6kgLg5iGNjiRsXTQr/RSNO+fPuZjoQW9U6u+jBzUXL7pFniaHQ/j0yH6hV3m/X4eZ9U5DuXVp4ENAYQ0cH6rkEEept++ufEl9Sz7UzQFvaqPcnzZhKzSiCro+mBjIt0+RZjqvT8PKatjZso2eXcGjLw/dsv28SHrPhh+N/iA+dlRzeGJUGpG00Yr5H'  
 S3_LOCATION = f'http://{S3_BUCKET}.s3.amazonaws.com/'
-API_ENDPOINT = "https://re7padjoo7.execute-api.us-east-1.amazonaws.com/get-data" 
+# API_ENDPOINT = "https://re7padjoo7.execute-api.us-east-1.amazonaws.com/get-data" 
 
 @app.route('/')
 def index():
